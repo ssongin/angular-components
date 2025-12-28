5119117938c351b3578acd36eefdd26e382e264b
Stanislav Songin <ssongin@gmail.com>
2025-12-28 21:54:42 +0200

    fix: Change docusaurus base url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://ssongin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/angular-components/',
+  baseUrl: '/angular-components/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
